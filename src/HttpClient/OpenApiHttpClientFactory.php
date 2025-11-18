822b19d09ae417b235dadbb8af4461676940d306
小周 <slps970093@gmail.com>
2025-11-18 10:59:11 +0800

    [openid-client] ssl verify options

--- src/HttpClient/OpenApiHttpClientFactory.php
+++ src/HttpClient/OpenApiHttpClientFactory.php
@@ -9,9 +9,16 @@ class OpenApiHttpClientFactory
 {
     private $openApiBaseUrl = "";
 
+    private $sslVerify = true;
     public function __construct($openApiBaseUrl)
     {
         $this->openApiBaseUrl = $openApiBaseUrl;
+    }
+
+    public function setSSLVerify($bool)
+    {
+        $this->sslVerify = $bool;
+        return $this;
     }
 
     public function makeOpenApiAuthClient($accessToken)
@@ -23,7 +30,7 @@ class OpenApiHttpClientFactory
                 'Content-Type' => 'application/json',
                 'Authorization' => 'Bearer ' . $accessToken
             ],
-                'verify' => false,  // 開發可用 略過 SSL
+            'verify' => $this->sslVerify,  // 開發可用 略過 SSL
         ]);
     }
 }
